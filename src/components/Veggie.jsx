719d2b8b0ad16ee7fecad9720b9f983e96647d5f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-08 13:29:47 -0400

    update vegetarian component to return veggie recipes

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -5,28 +5,28 @@ import "@splidejs/splide/dist/css/splide.min.css";
 
 
 export default function Veggie() {
-  const [popular, setPopular] = useState([]);
+  const [veggie, setVeggie] = useState([]);
 
   useEffect(() => {
-    getPopular();
+    getVeggie();
   }, [])
 
-  const getPopular = async () => {
+  const getVeggie = async () => {
 
     // checks local storage (cache) so that API is not refreshed every time (local storage only saves strings)
-    const checkStorage = localStorage.getItem('popular');
+    const checkStorage = localStorage.getItem('veggie');
 
     if (checkStorage) {
-      setPopular(JSON.parse(checkStorage))
+      setVeggie(JSON.parse(checkStorage))
     } else {
       const api = await fetch(
-        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`
       );
       const data = await api.json();
 
-      localStorage.setItem('popular', JSON.stringify(data.recipes))
+      localStorage.setItem('veggie', JSON.stringify(data.recipes))
       console.log(data.recipes)
-      setPopular(data.recipes)
+      setVeggie(data.recipes)
     }
   };
 
@@ -34,16 +34,16 @@ export default function Veggie() {
   return (
     <div>
       <Wrapper>
-        <h3>Popular Recipes</h3>
+        <h3>Vegetarian Recipes</h3>
         <Splide options={{
-          perPage: 4,
+          perPage: 3,
           arrows: false,
           pagination: false,
           drag: "free",
           gap: "5rem",
         }}
         >
-          {popular.map((recipe) => {
+          {veggie.map((recipe) => {
             return (
               <SplideSlide key={recipe.id}>
                 <Card>
