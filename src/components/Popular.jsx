dcb21534a46b1474684e02304801fae8027fc55c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-08 11:06:12 -0400

    add api request for popular recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,13 @@
 
 function Popular() {
+
+const getPopular = async () => {
+  const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REaCT_APP_API_KEY}&number=9`)
+}
+
   return (
     <div>
-      
+      Popular
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,16 @@
 import Popular from "../components/Popular";
 import Veggie from "../components/Veggie";
 
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <Popular />
+      <Veggie />
+      
+    </div>
+  )
+}
+
+export default Home
