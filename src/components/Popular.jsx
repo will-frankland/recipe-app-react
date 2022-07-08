d724caea0b406254f5593c8d007fd9f24804b6ad
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-08 13:23:04 -0400

    add Styles.jsx file to store styling components and C&P popular code into Veggie

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+import { Wrapper, Card, Gradient } from "./Styles";
 
 export default function Popular() {
   const [popular, setPopular] = useState([]);
@@ -27,8 +27,7 @@ export default function Popular() {
       console.log(data.recipes)
       setPopular(data.recipes)
     }
-
-  }
+  };
 
   return (
     <div>
@@ -58,48 +57,3 @@ export default function Popular() {
     </div>
   );
 }
-
-const Wrapper = styled.div`
-  margin: 4rem 0rem;
-`;
-
-const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2rem;
-  overflow: hidden;
-  position: relative;
-
-  img{
-    border-radius: 2rem;
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  p {
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    bottom: 0%;
-    transform: translate(-50%, 0%);
-    color: white;
-    width: 100%;
-    text-align: center;
-    font-weight: 600;
-    font-size: 1rem;
-    height; 40%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-
-const Gradient = styled.div`
-  z-index: 3;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0). rgba(0, 0, 0, 0.5));
-`;
--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,8 +1,61 @@
+import { Wrapper, Card, Gradient } from "./Styles";
+import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
 
 export default function Veggie() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, [])
+
+  const getPopular = async () => {
+
+    // checks local storage (cache) so that API is not refreshed every time (local storage only saves strings)
+    const checkStorage = localStorage.getItem('popular');
+
+    if (checkStorage) {
+      setPopular(JSON.parse(checkStorage))
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+
+      localStorage.setItem('popular', JSON.stringify(data.recipes))
+      console.log(data.recipes)
+      setPopular(data.recipes)
+    }
+  };
+
+
   return (
     <div>
-      Veggie
+      <Wrapper>
+        <h3>Popular Recipes</h3>
+        <Splide options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          gap: "5rem",
+        }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt="recipe.title" />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })};
+        </Splide>
+      </Wrapper>
     </div>
   )
 }
