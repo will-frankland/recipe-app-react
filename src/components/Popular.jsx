70c01e90b366c50ea3d9facac3c4eee84f9f6a73
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-08 12:53:01 -0400

    add styling to Card, add gradient component, add Splide sliding feature to recipe cards

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 export default function Popular() {
   const [popular, setPopular] = useState([]);
@@ -8,30 +10,38 @@ export default function Popular() {
     getPopular();
   }, [])
 
-const getPopular = async () => {
-  const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-  const data = await api.json()
-  console.log(data)
-  setPopular(data.recipes)
-}
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+    const data = await api.json()
+    console.log(data)
+    setPopular(data.recipes)
+  }
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return(
-          <Wrapper>
-            <h3>Popular Recipes</h3>
-            {popular.map((recipe) => {
-        return(
-          <Card>
-            <p>{recipe.title}</p>
-            <img src={recipe.image} alt="recipe.title" />
-          </Card>
-        );
-            })};
-          </Wrapper>
-        );
-      })}
+      <Wrapper>
+        <h3>Popular Recipes</h3>
+        <Splide options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          gap: "5rem",
+        }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt="recipe.title" />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })};
+        </Splide>
+      </Wrapper>
     </div>
   );
 }
@@ -43,4 +53,40 @@ const Wrapper = styled.div`
 const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img{
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height; 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0). rgba(0, 0, 0, 0.5));
 `;
