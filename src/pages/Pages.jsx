8bb4255ca16abf83027adf31bc228166f9f571f2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-09 13:58:10 -0400

    add browserRouter to Pages and create Cuisine Page

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import Home from './Home'
+import React from 'react';
+import Home from './Home';
+import Cuisine from './Cuisine';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 export default function Pages() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine" element={<Cuisine />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
