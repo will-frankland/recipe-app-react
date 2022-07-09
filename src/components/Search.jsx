f781828158529917e70d3b83e73e98b7ef9d783b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-09 14:38:09 -0400

    add styling to FormStyle for search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { FormStyle } from './Styles';
 
 export default function Search() {
   return (
     <div>
-      
+      <form>
+        <input type="text" />
+      </form>
     </div>
   )
 }
--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -82,9 +82,32 @@ export const SLink = styled(NavLink)`
     svg{
       color: white;
     }
-    
+
     h4 {
       color: white;
     }
+  }
+`;
+
+export const FormStyle = styled.div`
+  margin: 0rem 20rem;
+  position: relative;
+  width: 100%
+  input {
+    border: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1.5rem;
+    color: white;
+    padding; 1rem 3rem;
+    border: none;
+    border-radius: 1rem;
+    outline: none;
+  }
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+    color: white;
   }
 `;
