569972531d43de85fcff169e13130cb1963dd1fb
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-11 13:13:02 -0400

    add useEffect to searched.js functionality

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Searched() {
 
   const [searchedRecipes, setSearchedRecipes] = useState([]);
+  let params = useParams();
 
   const getSearched = async(name) => {
     const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`)
     const recipes = await data.json();
     setSearchedRecipes(recipes.results);
   };
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
 
   return (
     <div>
