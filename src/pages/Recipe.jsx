700906802400b5a259e17488f32911dd6b1f7919
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-13 14:50:08 -0400

    create Recipe page

---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+
+import React from 'react'
+
+function Recipe() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
