2a6016c897fb4069d5e3af88abdfc10098d657ba
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-07-13 14:53:35 -0400

    add recipe to pages render

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './Home';
 import Cuisine from './Cuisine';
 import Searched from './Searched';
+import Recipe from './Recipe';
 import { Route, Routes } from 'react-router-dom';
 
 export default function Pages() {
@@ -10,6 +11,7 @@ export default function Pages() {
         <Route path="/" element={<Home />} />
         <Route path="/cuisine/:type" element={<Cuisine />} />
         <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:id" element={<Recipe />} />
       </Routes>
   )
 }
